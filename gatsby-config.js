1391d32c9436fe736bf72a8e74ab0564ff9eb014
Luke Secomb <13529535+lukethacoder@users.noreply.github.com>
2018-03-22 23:01:28 +1100

    added work component

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,6 +4,28 @@ module.exports = {
   },
   plugins: [
     'gatsby-plugin-react-helmet',
-    'gatsby-plugin-styled-components'
+    'gatsby-plugin-styled-components',
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "src",
+        path: `${__dirname}/src/`
+      }
+    },
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "img",
+        path: `${__dirname}/src/images`
+      }
+    },
+    {
+      resolve: "gatsby-transformer-remark",
+      options: {
+        excerpt_separator: `<!-- end -->`
+      }
+    },
+    "gatsby-transformer-sharp",
+    "gatsby-plugin-sharp"
   ],
 };
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,23 +3,28 @@ import Link from 'gatsby-link'
 import styled from 'styled-components'
 
 import LandingPage from '../components/LandingPage';
+import { primaryWhite, primaryDarkGrey } from '../theme/variables';
 
 const IndexPageContainer = styled.div` 
   width: 100%;
   height: 100%;
   margin: 0;
-  background-color: ${props => props.theme.ofw};
-  h1 {
-    color: ${props => props.theme.dgr};
-  }
+  background-color: ${primaryDarkGrey};
 `;
+
+const WorkContainer = styled.div`
+  background-color: ${primaryWhite};
+  padding: 5% 0;
+`
 
 const IndexPage = () => (
   <IndexPageContainer>
     <LandingPage />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <WorkContainer>
+      <div>
+
+      </div>
+    </WorkContainer>
     <Link to="/resume/">Go to page 2</Link>
   </IndexPageContainer>
 )
