ac9bfb42e61c8d7aab88983d1a13d2d8b3a8a110
Luke Secomb <13529535+lukethacoder@users.noreply.github.com>
2018-03-30 01:25:42 +1100

    try again - moved gatsby-sharp above remark images

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,8 @@ module.exports = {
   plugins: [
     'gatsby-plugin-react-helmet',
     'gatsby-plugin-styled-components',
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
     {
       resolve: "gatsby-source-filesystem",
       options: {
@@ -34,8 +36,6 @@ module.exports = {
           }
         ]
       }
-    },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`
+    }
   ],
 };
