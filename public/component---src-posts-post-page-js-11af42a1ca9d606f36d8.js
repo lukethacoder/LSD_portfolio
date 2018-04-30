83636d09d81e46dec444d4bc05e3b3a9346199e2
Luke Secomb <13529535+lukethacoder@users.noreply.github.com>
2018-04-30 22:11:06 +1000

    delete public folder

--- public/component---src-posts-post-page-js-11af42a1ca9d606f36d8.js
+++
@@ -1,2 +0,0 @@
-webpackJsonp([88605697669780],{229:function(c,n){"use strict"}});
-//# sourceMappingURL=component---src-posts-post-page-js-11af42a1ca9d606f36d8.js.map
--- public/path---posts-test-post-725486201815e4dfca8f.js
+++
@@ -1,2 +0,0 @@
-webpackJsonp([0x89679c5af72b],{345:function(t,s){t.exports={pathContext:{slug:"/posts/test_post/"}}}});
-//# sourceMappingURL=path---posts-test-post-725486201815e4dfca8f.js.map
